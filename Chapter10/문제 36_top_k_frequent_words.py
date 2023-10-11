9866fe3f79eae7e1cce6b1ebe0c2d2b01c8730f7
roy <roiei.sw@gmail.com>
2023-10-11 19:29:55 +0900

    fixed code for covering newly added test cases

--- Chapter10/문제 36_top_k_frequent_words.py
+++ Chapter10/문제 36_top_k_frequent_words.py
@@ -8,7 +8,26 @@ r = 3
 def pick_frequent_words(nums: [str], k: int) -> [str]:
     freq = collections.Counter(nums)
     freq = sorted(freq.items(), key=lambda p: (p[1], p[0]), reverse=True)
-    return sorted([item[0] for item in freq[:k]])
+
+    words_in_freq = collections.defaultdict(list)
+
+    for word, cnt in freq:
+        words_in_freq[cnt] += word,
+
+    res = []
+    all_freqs = sorted(words_in_freq.keys(), reverse=True)
+    
+    for cur_freq in all_freqs:
+        num_words = len(words_in_freq[cur_freq])
+
+        if num_words <= k:
+            res += sorted(words_in_freq[cur_freq])[:num_words]
+            k -= num_words
+        else:
+            res += sorted(words_in_freq[cur_freq])[:k]
+            k -= k
+    
+    return res
 
 
 print(pick_frequent_words(words, r) == ['code', 'good', 'some'])
